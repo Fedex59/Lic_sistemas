52c2dc45ebe28102d75d1388a198d6a2921ca35b
Federico <pastranam.federico@gmail.com>
2024-11-19 16:19:24 -0300

    ejer 3

---
+++ 1_Primer_año/Ejercicio_3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) 
+{
+	int Fecha, Dia, Mes, Anio; 
+	cout<< "ingrese la hora en formato MMDDAA: ";
+	cin>>Fecha;
+	Anio = Fecha/10000;
+	Mes = Fecha%100;
+	Dia = (Fecha/100)%100;
+	cout <<"La fecha es: " << Dia <<"/"<< Mes <<"/"<< Anio ;
+	return 0;
