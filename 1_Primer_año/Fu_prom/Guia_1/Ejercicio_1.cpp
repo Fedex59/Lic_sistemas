a0e8e642caaa30ff318cecdae26a83cafe702b1c
Federico <pastranam.federico@gmail.com>
2024-12-01 20:50:03 -0300

    Ejer2_Guia1

--- 1_Primer_año/Fu_prom/Guia_1/Ejercicio_1.cpp
+++ 1_Primer_año/Fu_prom/Guia_1/Ejercicio_1.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int main(int argc, char const *argv[]) 
 {
 	int Plata; 
-	float Cotizacion, Dolar, Euro, Real;
+	double Cotizacion, Dolar, Euro, Real;
 	cout<< "Ingrese la cantidad de plata en pesos ";
 	cin>> Plata;
-    cout<< "Ingrese la cotizacion del dolar ";
+	cout<< "Ingrese la cotizacion del dolar ";
 	cin>>Cotizacion;
-	Dolar = Dolar*Cotizacion);
-    Euro=(Euro*105.49)*Cotizacion;
-    Real=(Real/16.74)*Cotizacion
-
-	cout <<"El valor del Dolar, Euro y Real es: " << Dolar<<" "<<Euro<<" "<<Real;
+	Dolar = Dolar*Cotizacion;
+	Euro=(Euro*105.49)*Cotizacion;
+	Real=(Real/16.74)*Cotizacion;
+		
+		cout <<"El valor del Dolar, Euro y Real es: " << Dolar<<" "<<Euro<<" "<<Real;
 	
 	
 	return 0;
