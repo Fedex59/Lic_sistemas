faef3b477f6acc6da7d129fbe19d655b38245d7e
Federico <pastranam.federico@gmail.com>
2024-12-06 17:08:39 -0300

    Ejer4 corr

---
+++ 1_Primer_año/Fu_prom/Guia_1/Ejercicio_4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) 
+{
+	int Comun, Estudiante, Trabajador; 
+    int Comun2, Trabajador2, Estudiante2;
+	float BolComun, BolEstudiante, BolTrabajador;
+	cout<< "Ingrese el valor del boleto Estudiante ";
+	cin>> BolEstudiante;
+	cout<< "Ingrese La cantidad inicial y final de boletos Estudiante ";
+	cin>> Estudiante;
+    cin>> Estudiante2;
+    cout<< "Ingrese La cantidad inicial y final de boletos Trabajador ";
+	cin>> Trabajador;
+    cin>> trabajador2;
+    cout<< "Ingrese La cantidad inicial y final de boletos Comun ";
+	cin>> Comun;
+    cin>> Comun2;
+
+    Comun= Comun2 - Comun;
+    Estudiante = Estudiante2 - Estudiante;
+    Trabajador = Trabajador2 - Trabajador;
+    
+    
+	
+	return 0;
+}
