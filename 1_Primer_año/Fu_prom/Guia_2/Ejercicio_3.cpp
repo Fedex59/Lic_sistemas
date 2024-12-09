c09523735a571f54c4ac6378e0595da78c49580f
Federico <pastranam.federico@gmail.com>
2024-12-09 18:45:39 -0300

    Guia3

--- 1_Primer_año/Fu_prom/Guia_2/Ejercicio_3.cpp
+++ 1_Primer_año/Fu_prom/Guia_2/Ejercicio_3.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
-
 using namespace std;
 
-int main(int argc, char const *argv[]) 
+int main(int argc, char const *argv[])
 {
-	string Nombre1, Nombre2, Nombre3; 
-	cout<< "Ingrese el primer nombre ";
-	cin>> Nombre1;
-	cout<< "Ingrese el Segundo nombre ";
-	cin>> Nombre2;
-    cout<< "Ingrese el Tercer nombre ";
-	cin>> Nombre3;
+	float Peso, Estatura;
+	string Nombre;
+	cout<< "ingrese su nombre: ";
+	cin>>Nombre;
+	cout<< "ingrese su peso: ";
+	cin>>Peso;
+	cout<< "ingrese su estatura: ";
+	cin>>Estatura;
+	Peso=(Peso/Estatura);
 	
-	if (Nombre1 > Nombre2) 
+	
+	if (Peso>0 & Peso<18,5)
 	{
-	swap(Nombre1, Nombre2);
+		cout <<" El indice de masa corporal de "<< Nombre <<" es " << Peso <<" y esta debajo del peso normal" ;
 	}
-	if (Nombre2 > Nombre3)
+	else  if (Peso>18,5 & Peso<25)
 	{
-		swap(Nombre2, Nombre3);
-    }
-   if (Nombre1 > Nombre2)
-    {
-        swap(Nombre1, Nombre2);
-    }    
-	cout << "Los nombres en orden alfabético son: " << endl;
-    cout << Nombre1 << endl;
-    cout << Nombre2 << endl;
-    cout << Nombre3 << endl;
-
-    return 0;
+		cout <<" El indice de masa corporal de "<< Nombre <<" es " << Peso <<" y esta dentro del peso normal" ;
+	}
+	else  if (Peso>25 & Peso<30)
+	{
+		cout <<" El indice de masa corporal de "<< Nombre <<" es " << Peso <<" y esta con sobrepeso" ;
+	}
+	else 
+	{
+		cout <<" El indice de masa corporal de "<< Nombre <<" es " << Peso <<" y esta con obesidad" ;
+	}
+	
+return 0;
 }
---
+++ 1_Primer_año/Fu_prom/Guia_2/Ejercicio_5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	float Peso, Estatura;
+	string Nombre;
+	cout<< "ingrese su nombre: ";
+	cin>>Nombre;
+	cout<< "ingrese su peso: ";
+	cin>>Peso;
+	cout<< "ingrese su estatura: ";
+	cin>>Estatura;
+	Peso=(Peso/Estatura);
+	
+	
+    if (Peso>0&Peso<18,5)
+    {
+        cout <<" El indice de masa corporal de "<< Nombre <<"es " << Peso <<"y esta debajo del peso normal" ;
+    }
+    else  if (Peso>18,5&Peso<25)
+    {
+        cout <<" El indice de masa corporal de "<< Nombre <<"es " << Peso <<"y esta dentro del peso normal" ;
+    }
+    else  if (Peso>25&Peso<30)
+    {
+        cout <<" El indice de masa corporal de "<< Nombre <<"es " << Peso <<"y esta con sobrepeso" ;
+    }
+    else 
+    {
+        cout <<" El indice de masa corporal de "<< Nombre <<"es " << Peso <<"y esta con obesidad" ;
+    }
+
+    return 0;
+}
