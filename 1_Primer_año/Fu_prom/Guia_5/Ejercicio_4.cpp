ccdebcc35cb57876f4282e51a5b2f640469b94da
Fede_pastrana <126290811+Fedex59@users.noreply.github.com>
2025-01-29 18:40:20 -0300

    Update Ejercicio_4.cpp

---
+++ 1_Primer_año/Fu_prom/Guia_5/Ejercicio_4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) 
+{
+	int num[5],num2[5], i; 
+	
+	
+	for (i=0;i<5 ; i++) {
+		cout << "Ingrese 5 numeros: ";
+		cin >> num[i];
+	}
+	for (i=0;i<5 ; i++) {
+		num2[i]=num[i]*3;
+		}
+	for (i=0;i<5 ; i++) {
+		cout << "Los numeros multiplicados son: "<<num2[i]<<endl;
+		}
+	return 0;
+}
