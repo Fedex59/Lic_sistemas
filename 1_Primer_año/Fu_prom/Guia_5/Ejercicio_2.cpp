ec7351a763584b8b837f6989502651410504ea90
Federico <pastranam.federico@gmail.com>
2025-01-29 18:22:29 -0300

    ejercicio 2 guia 5

---
+++ 1_Primer_año/Fu_prom/Guia_5/Ejercicio_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) 
+{
+	int num[10], max, min, par, valor; 
+		
+	for (i=0;i<10 ; i++) {
+		cout << "Ingrese 10 numeros: ";
+		cin >> num[i];
+	}
+	
+	
+	
+	return 0;
+}
