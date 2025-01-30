abaefcd2bdc17df18478f14d59b4316a5dbc4b5f
Federico <pastranam.federico@gmail.com>
2025-01-30 17:11:06 -0300

    acodando ejercicios 3 y 4

--- 1_Primer_año/Fu_prom/Guia_5/Ejercicio_3.cpp
+++ 1_Primer_año/Fu_prom/Guia_5/Ejercicio_3.cpp
@@ -19,25 +19,3 @@ int main(int argc, char const *argv[])
 	
 	return 0;
 }
-=======
-#include <iostream>
-
-using namespace std;
-
-int main(int argc, char const *argv[]) 
-{
-	int num[10], i; 
-	
-	
-	for (i=0;i<10 ; i++) {
-		cout << "Ingrese 10 numeros: ";
-		cin >> num[i];
-	}
-	
-	for (i=9;i>=0;i--) {
-		cout << "los numeros son: "<<num[i]<<endl;
-		}
-	
-	return 0;
-}
->>>>>>> a7d4825260648dea76cc48bfae4dbe9283a7d841
