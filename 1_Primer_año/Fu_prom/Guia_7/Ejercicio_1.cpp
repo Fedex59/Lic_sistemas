5174c0c847ce0de17cb9acc158d302f53c193dc6
Federico <pastranam.federico@gmail.com>
2025-07-27 18:58:42 -0300

    subalg

--- 1_Primer_año/Fu_prom/Guia_7/Ejercicio_1.cpp
+++ 1_Primer_año/Fu_prom/Guia_7/Ejercicio_1.cpp
@@ -2,21 +2,31 @@
 #include <cmath>
 using namespace std;
 
-//float Hipot(int ladoa, int ladob);
-float Facto(int FA);
-//int Division_entera(int DA, int DB);
-//void Intercambio(char &IA, char &IB);
-//int Suma_vector(int Vec[], int Cant);
+float Hipot(int ladoa, int ladob);
+int Facto(int FA);
+int Division_entera(int DA, int DB);
+void Intercambio(char &IA, char &IB);
+int Suma_vector(int Vec[], int Cant);
 
 int main(int argc, char *argv[]) {
 	
-	int FA;
-	//float resultado;
-		
-	cout <<"ingrese los valores de A y B ";
-	cin>> FA;
+	int ladoa, ladob, FA, Fac,  DA, DB, Res, cociente, Cant, suma_v;
+	float resultado;
+	char IA, IB, inter;
+	int Vec[100]
 	
-	//resultado = Hipot(a, b);
+	//Cant = cantidad de elementos del vector
+
+	resultado = Hipot(a, b);
+
+	Fac= Facto(FA);
+
+	cociente = Division_entera(DA, DB, Res);
+
+	inter = Intercambio (IA, IB);
+
+	suma_v = Suma_vector(Vec [], Cant);
+
 	
 	cout<< "el resultado de la hipotenusa es: "<< FA; 
 	
@@ -24,19 +34,50 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-/*float Hipot(int ladoa, int ladob)
+float Hipot(int ladoa, int ladob)
 {
-	
-	float resultado = (ladoa*ladoa)+(ladob*ladob)  ;
-	
-	return sqrt (resultado);
-	
-}*/
 
-float Facto(int FA);{
+float resultado = (ladoa*ladoa)+(ladob*ladob)  ;
 
-for (int i=FA;i>=1;i--)
-	FA = FA * i;
-	return (FA);
+return sqrt (resultado);
+
 }
 
+int Facto(int FA) {
+	int resultado = 1;
+	for (int i = FA; i >= 1; i--) {
+		resultado *= i;
+	}
+	return resultado;
+}
+
+
+int Division_entera(int DA, int DB, int &Res)
+{
+	int resultado;
+
+	resultado = DA/DB;
+	
+	Res= DA%DB;
+
+
+	return resultado;
+}
+
+void Intercambio(char &IA, char &IB){
+
+	char aux;
+	aux = IA;
+	IA = IB;
+	IB = aux; 
+}
+
+int Suma_vector(int Vec[], int Cant){
+		int suma=0;
+
+	for (int i=0; i<Cant; i++)
+	{
+		suma= suma + Vec[i];
+	}
+	return (suma);
+}
